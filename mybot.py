8a8fa0bbc7282377264f828dd9a0a5946ff92bc5
LeraBulanova <lera_bulanova@list.ru>
2021-12-22 02:31:17 +0300

    New version of bot

--- mybot.py
+++ mybot.py
@@ -2,12 +2,23 @@ import telebot
 
 bot = telebot.TeleBot("5023155853:AAGGj1lPuILWqrK-1H_EUp0i5zUt1EUvPBY")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+        markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=telebot.types.KeyboardButton("Собачка")
+        item2=telebot.types.KeyboardButton("Кошечка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, "Как твое настроение? Надеюсь хорошо. Чтобы стало еще лучше я покажу тебе милое фото\n")
+        bot.send_message(m.chat.id, 'Нажми "собачка", чтобы получить фото собачки или "кошечка", чтобы получить фото кошечки',  reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
-bot.infinity_polling()
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text.strip() == 'Собачка' :
+            photo = open('dog.png', 'rb')
+            bot.send_photo(message.chat.id, photo)
+    elif message.text.strip() == 'Кошечка':
+            photo = open('cat.jpg', 'rb')
+            bot.send_photo(message.chat.id, photo)
+    bot.send_message(message.chat.id, "Хорошего дня!")
+bot.polling(none_stop=True, interval=0)
