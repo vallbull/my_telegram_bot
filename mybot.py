0eb410f11df295770881ecaa81277201d906ac94
LeraBulanova <lera_bulanova@list.ru>
2021-12-24 00:31:42 +0300

    Fixed bugs

--- mybot.py
+++ mybot.py
@@ -1,18 +1,20 @@
+'''my first bot'''
+
 import telebot
 
 bot = telebot.TeleBot("5023155853:AAGGj1lPuILWqrK-1H_EUp0i5zUt1EUvPBY")
 
 @bot.message_handler(commands=["start"])
-def start(m):
+def start(message):
     '''Add keyboar with two buttons and create question'''
     markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=telebot.types.KeyboardButton("Собачка")
     item2=telebot.types.KeyboardButton("Кошечка")
     markup.add(item1)
     markup.add(item2)
-    bot.send_message(m.chat.id, "Как твое настроение? Надеюсь, хорошо. \
+    bot.send_message(message.chat.id, "Как твое настроение? Надеюсь, хорошо. \
         Чтобы стало еще лучше, я покажу тебе милое фото\n")
-    bot.send_message(m.chat.id, 'Нажми "собачка", чтобы получить фото собачки,\
+    bot.send_message(message.chat.id, 'Нажми "собачка", чтобы получить фото собачки,\
         или "кошечка", чтобы получить фото кошечки',  reply_markup=markup)
 
 @bot.message_handler(content_types=["text"])
